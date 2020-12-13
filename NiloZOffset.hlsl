b461d70a5bc790fc74361edd695c54f684cd821c
ColinLeung-NiloCat <7102676+ColinLeung-NiloCat@users.noreply.github.com>
2020-12-13 19:47:27 +0800

    fix ZOffset bug on OpenGL -like platform

--- NiloZOffset.hlsl
+++ NiloZOffset.hlsl
@@ -1,4 +1,4 @@
-﻿// For more information, visit -> https://github.com/ColinLeung-NiloCat/UnityURPToonLitShaderExample
+// For more information, visit -> https://github.com/ColinLeung-NiloCat/UnityURPToonLitShaderExample
 
 #ifndef Include_NiloZOffset
 #define Include_NiloZOffset
@@ -13,25 +13,34 @@
 // -Solve ZFighting issue without moving geometry
 float4 NiloGetNewClipPosWithZOffset(float4 originalPositionCS, float viewSpaceZOffsetAmount)
 {
-	if(unity_OrthoParams.w == 0)
-	{
-		////////////////////////////////
-		//Perspective camera case
-		////////////////////////////////
-		float2 ProjM_ZRow_ZW = UNITY_MATRIX_P[2].zw;
-		float modifiedPositionVS_Z = -originalPositionCS.w + -viewSpaceZOffsetAmount; // push imaginary vertex
-		float modifiedPositionCS_Z = modifiedPositionVS_Z * ProjM_ZRow_ZW[0] + ProjM_ZRow_ZW[1];
-		originalPositionCS.z = modifiedPositionCS_Z * originalPositionCS.w / (-modifiedPositionVS_Z); // overwrite positionCS.z
-		return originalPositionCS;	
-	}
-	else
-	{
-		////////////////////////////////
-		//Orthographic camera case
-		////////////////////////////////
-		originalPositionCS.z += -viewSpaceZOffsetAmount / _ProjectionParams.z; // push imaginary vertex and overwrite positionCS.z
-		return originalPositionCS;
-	}
+    /*
+    https://docs.unity3d.com/Manual/SL-PlatformDifferences.html
+
+    Direct3D-like: The clip space depth goes from +1.0 at the near plane to 0.0 at the far plane. This applies to Direct3D, Metal and consoles.
+    OpenGL-like: The clip space depth goes from –1.0 at the near plane to +1.0 at the far plane. This applies to OpenGL and OpenGL ES.
+    Inside Shader code, you can use the UNITY_NEAR_CLIP_VALUE built-in macro to get the near plane value based on the platform.
+    */
+    viewSpaceZOffsetAmount *= UNITY_NEAR_CLIP_VALUE; //if OpenGL-like platform, we * -1 to viewSpaceZOffsetAmount
+
+    if(unity_OrthoParams.w == 0)
+    {
+        ////////////////////////////////
+        //Perspective camera case
+        ////////////////////////////////
+        float2 ProjM_ZRow_ZW = UNITY_MATRIX_P[2].zw;
+        float modifiedPositionVS_Z = -originalPositionCS.w + -viewSpaceZOffsetAmount; // push imaginary vertex
+        float modifiedPositionCS_Z = modifiedPositionVS_Z * ProjM_ZRow_ZW[0] + ProjM_ZRow_ZW[1];
+        originalPositionCS.z = modifiedPositionCS_Z * originalPositionCS.w / (-modifiedPositionVS_Z); // overwrite positionCS.z
+        return originalPositionCS;    
+    }
+    else
+    {
+        ////////////////////////////////
+        //Orthographic camera case
+        ////////////////////////////////
+        originalPositionCS.z += -viewSpaceZOffsetAmount / _ProjectionParams.z; // push imaginary vertex and overwrite positionCS.z
+        return originalPositionCS;
+    }
 }
 
 #endif
