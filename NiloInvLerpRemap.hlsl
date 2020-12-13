2e1d5b8a0cee98c75015c96ba98bdf9c38b2f1b3
ColinLeung-NiloCat <7102676+ColinLeung-NiloCat@users.noreply.github.com>
2020-12-13 20:07:09 +0800

    indentation

--- NiloInvLerpRemap.hlsl
+++ NiloInvLerpRemap.hlsl
@@ -7,16 +7,16 @@
 // just like smoothstep(), but linear, not clamped
 half invLerp(half from, half to, half value) 
 {
-	return (value - from) / (to - from);
+    return (value - from) / (to - from);
 }
 half invLerpClamp(half from, half to, half value)
 {
-	return saturate(invLerp(from,to,value));
+    return saturate(invLerp(from,to,value));
 }
 // full control remap, but slower
 half remap(half origFrom, half origTo, half targetFrom, half targetTo, half value)
 {
-	half rel = invLerp(origFrom, origTo, value);
-	return lerp(targetFrom, targetTo, rel);
+    half rel = invLerp(origFrom, origTo, value);
+    return lerp(targetFrom, targetTo, rel);
 }
 #endif
