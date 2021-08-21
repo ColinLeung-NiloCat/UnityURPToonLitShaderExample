d2137f2602b7a2249c2a432524151eea56c075c1
ColinLeung-NiloCat <7102676+ColinLeung-NiloCat@users.noreply.github.com>
2021-08-21 18:58:06 +0800

    Rename LightingData to ToonLightingData
    to avoid naming conflict with URP12

--- SimpleURPToonLitOutlineExample_LightingEquation.hlsl
+++ SimpleURPToonLitOutlineExample_LightingEquation.hlsl
@@ -7,7 +7,7 @@
 // doing this can make sure your .hlsl's user can include this .hlsl anywhere anytime without producing any multi include conflict
 #pragma once
 
-half3 ShadeGI(ToonSurfaceData surfaceData, LightingData lightingData)
+half3 ShadeGI(ToonSurfaceData surfaceData, ToonLightingData lightingData)
 {
     // hide 3D feeling by ignoring all detail SH (leaving only the constant SH term)
     // we just want some average envi indirect color only
@@ -23,7 +23,7 @@ half3 ShadeGI(ToonSurfaceData surfaceData, LightingData lightingData)
 
 // Most important part: lighting equation, edit it according to your needs, write whatever you want here, be creative!
 // This function will be used by all direct lights (directional/point/spot)
-half3 ShadeSingleLight(ToonSurfaceData surfaceData, LightingData lightingData, Light light, bool isAdditionalLight)
+half3 ShadeSingleLight(ToonSurfaceData surfaceData, ToonLightingData lightingData, Light light, bool isAdditionalLight)
 {
     half3 N = lightingData.normalWS;
     half3 L = light.direction;
@@ -58,13 +58,13 @@ half3 ShadeSingleLight(ToonSurfaceData surfaceData, LightingData lightingData, L
     return saturate(light.color) * lightAttenuationRGB * (isAdditionalLight ? 0.25 : 1);
 }
 
-half3 ShadeEmission(ToonSurfaceData surfaceData, LightingData lightingData)
+half3 ShadeEmission(ToonSurfaceData surfaceData, ToonLightingData lightingData)
 {
     half3 emissionResult = lerp(surfaceData.emission, surfaceData.emission * surfaceData.albedo, _EmissionMulByBaseColor); // optional mul albedo
     return emissionResult;
 }
 
-half3 CompositeAllLightResults(half3 indirectResult, half3 mainLightResult, half3 additionalLightSumResult, half3 emissionResult, ToonSurfaceData surfaceData, LightingData lightingData)
+half3 CompositeAllLightResults(half3 indirectResult, half3 mainLightResult, half3 additionalLightSumResult, half3 emissionResult, ToonSurfaceData surfaceData, ToonLightingData lightingData)
 {
     // [remember you can write anything here, this is just a simple tutorial method]
     // here we prevent light over bright,
